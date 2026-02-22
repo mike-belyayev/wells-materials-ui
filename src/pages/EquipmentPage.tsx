bebd10c71b3065b58ad64fdba7cafac093093889
Mike Belyayev <4xgood@gmail.com>
2026-02-22 10:26:25 -0500

    removed unused declarations

--- src/pages/EquipmentPage.tsx
+++ src/pages/EquipmentPage.tsx
@@ -20,7 +20,7 @@ const EquipmentPage = () => {
 
   // State
   const [currentLocation, setCurrentLocation] = useState(user?.homeLocation || 'NSC');
-  const [setSites] = useState<Site[]>([]);
+  const [_sites, setSites] = useState<Site[]>([]);
   const [allWells, setAllWells] = useState<Well[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
